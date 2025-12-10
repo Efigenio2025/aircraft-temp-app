bf2a1f8af5ca2f7fa43a4730553bf12332ea6597
Efigenio2025 <joshua.efigenio@icloud.com>
2025-12-10 12:35:03 -0600

    Fix linting for API functions

--- api/NightTailsCreate/index.js
+++ api/NightTailsCreate/index.js
@@ -36,7 +36,7 @@ module.exports = async function (context, req) {
 
     const now = new Date();
     const dateStr = dateOverride || now.toISOString().slice(0, 10);
-    const partitionKey = \`\${station}-\${dateStr}\`;
+    const partitionKey = `${station}-${dateStr}`;
     const rowKey = uuidv4();
 
     const entity = {
